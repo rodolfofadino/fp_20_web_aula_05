3801d82dacb3d3f831d04a363ed5379742fcb63d
RodolfoFadino <rodolfo.fadino@gmail.com>
2020-04-04 16:21:13 -0300

    final wpo

--- fiapweb2020.core/Services/NoticiaService.cs
+++ fiapweb2020.core/Services/NoticiaService.cs
@@ -1,5 +1,7 @@
 ﻿using CodeHollow.FeedReader;
 using fiapweb2020.core.Models;
+using Microsoft.Extensions.Caching.Distributed;
+using Microsoft.Extensions.Caching.Memory;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,25 +10,40 @@ namespace fiapweb2020.core.Services
 {
     public class NoticiaService : INoticiaService
     {
+        private IMemoryCache _memoryCache;
 
-        public NoticiaService()
+        public NoticiaService(IMemoryCache memoryCache)
+        //public NoticiaService(IDistributedCache memoryCache)
         {
-
+            _memoryCache = memoryCache;
         }
 
         public List<Noticia> Load(int totalDeNoticias)
         {
-            var noticias = new List<Noticia>();
-            var feed = FeedReader.ReadAsync("https://g1.globo.com/rss/g1/turismo-e-viagem/").Result;
+            var key = $"noticias_";
 
-            foreach (var item in feed.Items)
+            List<Noticia> noticias;
+            
+            if (!_memoryCache.TryGetValue(key, out noticias))
             {
-                var feedItem = item.SpecificItem as CodeHollow.FeedReader.Feeds.MediaRssFeedItem;
-                var media = feedItem.Media;
-                var url = "";
-                if (media.Any())
-                    url = media.FirstOrDefault().Url;
-                noticias.Add(new Noticia() { Id = 1, Titulo = item.Title, Link = item.Link, Imagem = url });
+                noticias = new List<Noticia>();
+
+                var feed = FeedReader.ReadAsync("https://g1.globo.com/rss/g1/turismo-e-viagem/").Result;
+
+                foreach (var item in feed.Items)
+                {
+                    var feedItem = item.SpecificItem as CodeHollow.FeedReader.Feeds.MediaRssFeedItem;
+                    var media = feedItem.Media;
+                    var url = "";
+                    if (media.Any())
+                        url = media.FirstOrDefault().Url;
+                    noticias.Add(new Noticia() { Id = 1, Titulo = item.Title, Link = item.Link, Imagem = url });
+                }
+
+                var cacheOptions = new MemoryCacheEntryOptions().SetAbsoluteExpiration(DateTime.Now.AddMinutes(2));
+                //var cacheOptions = new MemoryCacheEntryOptions().SetSlidingExpiration( TimeSpan.FromMinutes(2));
+
+                _memoryCache.Set(key, noticias, cacheOptions);
             }
 
             return noticias;
---
+++ fiapweb2020/Startup.cs
@@ -0,0 +1,178 @@
+﻿using fiapweb2020.core.Contexts;
+using fiapweb2020.Middlewares;
+using fiapweb2020.core.Services;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.ResponseCompression;
+using Microsoft.Net.Http.Headers;
+
+namespace fiapweb2020
+{
+    public class Startup
+    {
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+            //services.AddTransient<INoticiaService, NoticiaService>();
+            //services.AddScoped<INoticiaService, NoticiaService>();
+            services.AddSingleton<INoticiaService, NoticiaService>();
+
+            //services.AddDataProtection()
+            //    .SetApplicationName("admin")
+            //    .PersistKeysToFileSystem(new DirectoryInfo(@"c:/teste/"));
+
+            services.AddDbContext<ClienteContext>(o => o.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=EFGetStarted.AspNetCore.Fiap;Trusted_Connection=True;ConnectRetryCount=0"));
+
+            services.Configure<GzipCompressionProviderOptions>(
+                o => o.Level = System.IO.Compression.CompressionLevel.Fastest);
+
+            services.AddResponseCompression(
+                o => o.Providers.Add<GzipCompressionProvider>());
+
+            services.AddAuthentication("app")
+                .AddCookie("app", o =>
+                {
+                    o.LoginPath = "/account/index";
+                    o.AccessDeniedPath = "/account/denied";
+                });
+
+            services.AddMemoryCache();
+
+            services.AddMvc();
+
+        }
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            #region middleware exemplos
+
+
+            // app.Use(async (context, next) =>
+            // {
+            //     //antes
+            //     await next.Invoke();
+            //     //depois
+            // });
+
+
+            // app.Map("/admin", mapApp =>
+            //{
+            //    mapApp.Run(async context =>
+            //    {
+            //        await context.Response.WriteAsync("Admin");
+            //    });
+            //});
+
+            // app.MapWhen(
+            //     context => context.Request.Query.ContainsKey("queryTeste"),
+            //     mapApp =>
+            //     {
+            //         mapApp.Run(async context =>
+            //         {
+            //             await context.Response.WriteAsync("Hello Fiap!");
+            //         });
+            //     }
+            // );
+
+
+
+            // app.Use(async (context, next) =>
+            // {
+            //     //antes
+            //     await next.Invoke();
+            //     //depois
+            // });
+
+            // app.Run(async context =>
+            // {
+            //     await context.Response.WriteAsync("Hello from 2nd delegate.");
+            // }
+            // );
+
+            #endregion
+
+            //app.UseRequestLocalization(new RequestLocalizationOptions
+            //{
+            //    DefaultRequestCulture = new RequestCulture("pt-br"),
+            //    // Formatting numbers, dates, etc.
+            //    SupportedCultures = supportedCultures,
+            //    // UI strings that we have localized.
+            //    SupportedUICultures = supportedCultures
+            //});
+
+            //app.UseMiddleware<MeuMiddleware>();
+
+
+            app.UseMeuMiddleware();
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/home/error");
+            }
+
+            //app.UseExceptionHandler(errorApp =>
+            //{
+            //    errorApp.Run(async context =>
+            //    {
+            //        context.Response.StatusCode = 500;
+            //        context.Response.ContentType = "text/html";
+
+            //        await context.Response.WriteAsync("<html lang=\"en\"><body>\r\n");
+            //        await context.Response.WriteAsync("ERROR!<br><br>\r\n");
+
+            //        var exceptionHandlerPathFeature =
+            //            context.Features.Get<IExceptionHandlerPathFeature>();
+
+            //        // Use exceptionHandlerPathFeature to process the exception (for example, 
+            //        // logging), but do NOT expose sensitive error information directly to 
+            //        // the client.
+
+            //        if (exceptionHandlerPathFeature?.Error is FileNotFoundException)
+            //        {
+            //            await context.Response.WriteAsync("File error thrown!<br><br>\r\n");
+            //        }
+
+            //        await context.Response.WriteAsync("<a href=\"/\">Home</a><br>\r\n");
+            //        await context.Response.WriteAsync("</body></html>\r\n");
+            //        await context.Response.WriteAsync(new string(' ', 512)); // IE padding
+            //    });
+            //});
+
+            app.UseResponseCompression();
+
+            app.UseAuthentication();
+
+            app.UseStaticFiles(new StaticFileOptions()
+            {
+                OnPrepareResponse = ctx =>
+                {
+                    var yearDurationInSeconds = 60 * 60 * 24* 365;
+                    ctx.Context.Response.Headers[HeaderNames.CacheControl] = $"public, max-age={yearDurationInSeconds}";
+                }
+            });
+
+
+
+            //app.UseResponseCompression();
+
+            app.UseMvc(
+                routes =>
+                {
+                    routes.MapRoute(
+                    name: "rota1",
+                    template: "modulo/{action=Index}/{id?}",
+                    defaults: new { controller = "Teste" });
+
+                    routes.MapRoute(
+                    name: "rota2",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+                });
+        }
+    }
+}
