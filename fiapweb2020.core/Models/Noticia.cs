3017f7ad2d108ebf44b048f5d06e8f2bb2e1d444
RodolfoFadino <rodolfo.fadino@gmail.com>
2020-04-04 08:32:04 -0300

    setup aula wpo

--- fiapweb2020.core/Models/Noticia.cs
+++ fiapweb2020.core/Models/Noticia.cs
@@ -2,8 +2,10 @@
 {
     public class Noticia
     {
-        public int Id { get;  set; }
-        public string Titulo { get;  set; }
-        public string Link { get;  set; }
+        public int Id { get; set; }
+
+        public string Titulo { get; set; }
+        public string Imagem { get; set; }
+        public string Link { get; set; }
     }
 }
--- fiapweb2020.core/Services/NoticiaService.cs
+++ fiapweb2020.core/Services/NoticiaService.cs
@@ -1,6 +1,8 @@
-﻿using fiapweb2020.core.Models;
+﻿using CodeHollow.FeedReader;
+using fiapweb2020.core.Models;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace fiapweb2020.core.Services
 {
@@ -15,14 +17,16 @@ namespace fiapweb2020.core.Services
         public List<Noticia> Load(int totalDeNoticias)
         {
             var noticias = new List<Noticia>();
-            for (int i = 0; i < totalDeNoticias; i++)
+            var feed = FeedReader.ReadAsync("https://g1.globo.com/rss/g1/turismo-e-viagem/").Result;
+
+            foreach (var item in feed.Items)
             {
-                noticias.Add(new Noticia()
-                {
-                    Id = i + 1,
-                    Titulo = $"Noticia sobre {i + 1}",
-                    Link = "http://www.globo.com"
-                });
+                var feedItem = item.SpecificItem as CodeHollow.FeedReader.Feeds.MediaRssFeedItem;
+                var media = feedItem.Media;
+                var url = "";
+                if (media.Any())
+                    url = media.FirstOrDefault().Url;
+                noticias.Add(new Noticia() { Id = 1, Titulo = item.Title, Link = item.Link, Imagem = url });
             }
 
             return noticias;
